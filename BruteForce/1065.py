cb34d3e04f09af39d2920ddfe4f4903d43627466
tail95 <xodlf8637@naver.com>
2019-11-18 21:10:35 +0900

    [BOJ]1065 한수

--- BruteForce/1065.py
+++ BruteForce/1065.py
@@ -14,7 +14,7 @@ for i in range(1, num+1):
 print(hansu)
 
 
--------------------------------------------------
+#-------------------------------------------------
 num = int(input())
 hansu = 0
 
@@ -23,6 +23,6 @@ for n in range(1, num+1) :
         hansu += 1 
     
     else :     
-        nums = list(map(int, str(n))) # 숫자를 자릿수대로 분리 
+        nums = list(map(int, str(n))) # 숫자를 자릿수대로 분리
         if nums[0] - nums[1] == nums[1] - nums[2] : #등차수열 확인
             hansu+=1

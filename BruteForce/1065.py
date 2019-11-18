ffb5f571bab9fe93400ac7400ceca007696a519b
tail95 <xodlf8637@naver.com>
2019-11-18 21:09:04 +0900

    한수 문제

---
+++ BruteForce/1065.py
@@ -0,0 +1,28 @@
+num = int(input())
+hansu = 0
+
+
+for i in range(1, num+1):
+    if i<100:
+        hansu+=1
+    else:
+        i = str(i)
+        tmp = int(i[1]) - int(i[0])
+        for j in range(1, len(i)-1):
+            if tmp == int(i[j+1]) - int(i[j]):
+                hansu+=1
+print(hansu)
+
+
+-------------------------------------------------
+num = int(input())
+hansu = 0
+
+for n in range(1, num+1) :
+    if n <= 99 : # 1부터 99까지는 모두 한수
+        hansu += 1 
+    
+    else :     
+        nums = list(map(int, str(n))) # 숫자를 자릿수대로 분리 
+        if nums[0] - nums[1] == nums[1] - nums[2] : #등차수열 확인
+            hansu+=1

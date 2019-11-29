673502641af7713399c6b6b865be899810c9e449
tail95 <xodlf8637@naver.com>
2019-11-29 22:26:42 +0900

    [Programmers]피보나치 수

---
+++ DynamicProgramming/피보나치 수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int arr[100001];
+
+int solution(int n) {
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;++i){
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+    }
+    return arr[n] % 1234567;
+}
+
+
+
+////////// 재밌는 파이썬풀이
+//def solution(n) :
+//	a, b = 0, 1
+//	for i in range(n) :
+//		a, b = b, (a + b) % 1234567
+//		return a % 1234567

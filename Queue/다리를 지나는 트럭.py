f418eef4eb338692d3e5594b9a4ee682eb008114
tail95 <xodlf8637@naver.com>
2020-01-06 20:14:28 +0900

    [Programmers]다리를 지나는 트럭

--- Queue/다리를 지나는 트럭.py
+++ Queue/다리를 지나는 트럭.py
@@ -5,24 +5,14 @@ def solution(bridge_length, weight, truck_weights):
     onBridge = deque([0 for _ in range(bridge_length)])
     sumOnBridge = 0
     while truck_weights:
+        front = onBridge.popleft()    
+        sumOnBridge -= front
         if sumOnBridge + truck_weights[-1] <= weight:
-            front = onBridge.popleft()
-            sumOnBridge -= front
             truck = truck_weights.pop()
             onBridge.append(truck)
             sumOnBridge += truck
         else:
-            front = onBridge.popleft()
-            sumOnBridge -= front
-            if front == 0 :
-                onBridge.append(front)
-            else:
-                if sum(onBridge) + truck_weights[-1] <= weight:
-                    truck = truck_weights.pop()
-                    onBridge.append(truck)
-                    sumOnBridge+=truck
-                else:
-                    onBridge.append(0)  
+            onBridge.append(0)
         # print(onBridge)
         # print(truck_weights)
         answer += 1
@@ -30,7 +20,10 @@ def solution(bridge_length, weight, truck_weights):
     return answer
 
 
-print(solution(5, 10, [5]))    # 6
-print(solution(2, 10, [7, 4, 5, 6]))
-print(solution(100,100,[10]))
-print(solution(100,100,[10,10,10,10,10,10,10,10,10,10]))
+# print(solution(5, 10, [5]))    # 6
+# print(solution(2, 10, [7, 4, 5, 6]))
+# print(solution(100,100,[10]))
+# print(solution(100,100,[10,10,10,10,10,10,10,10,10,10]))
+
+# 내장 함수 sum을 사용하면 시간 초과 (전체 deque에 대해 실행되므로)
+# 직접 다리위에 올라간 트럭들의 중량합을 더하고 빼서 구함(단순 덧셈)

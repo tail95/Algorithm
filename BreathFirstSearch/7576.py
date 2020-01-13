761b6c5cb86b1060234bff3d9d98233ec9770025
tail95 <xodlf8637@naver.com>
2020-01-13 16:39:27 +0900

    [BOJ]7576 토마토

--- BreathFirstSearch/7576.py
+++ BreathFirstSearch/7576.py
@@ -3,31 +3,25 @@ from collections import deque
 
 M, N = map(int, sys.stdin.readline().split())
 tomatos = []
-answer = 0
 for i in range(N):
     tomatos.append(list(map(int, sys.stdin.readline().split())))
 
-visited = [[False]*M for _ in range(N)]
 q = deque()
 for i in range(N):
     for j in range(M):
-        if tomatos[i][j] == 1 and not visited[i][j]:
+        if tomatos[i][j] == 1:
             q.append((i, j))
-            visited[i][j] = True
 while q:
-    # print(q)
     frontX, frontY = q.popleft()
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
     for i in range(4):
         nx = frontX + dx[i]
         ny = frontY + dy[i]
-        # print("nx: " + str(nx) + " ny: " + str(ny))
-        if 0 <= nx < N and 0 <= ny < M and tomatos[nx][ny] == 0 and not visited[nx][ny]:
-            # print("#"*30)
+        if 0 <= nx < N and 0 <= ny < M and tomatos[nx][ny] == 0:
             tomatos[nx][ny] = max(tomatos[frontX][frontY] + 1, tomatos[nx][ny])
             q.append((nx,ny))
-# print(tomatos)
+
 tCount = 0
 tMax = 0
 for t in tomatos:
@@ -37,3 +31,6 @@ if tCount>0:
     print(-1)
 else:
     print(tMax - 1)
+
+# 기존풀이: 1 상하 좌우로 1로 만듦 // 매 루프마다 1인것을 체크해 queue에 추가 ( 시간 소요 많음)
+# 해결: 토마토의 거리를 계산해 거리 값을 계산 후 max값으로 계산 

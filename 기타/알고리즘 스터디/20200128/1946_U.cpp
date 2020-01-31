aeec653be796005fbc74ce34ab7207e73a6902ec
tail95 <xodlf8637@naver.com>
2020-01-31 20:13:25 +0900

    [BOJ]1946 신입사원

--- 기타/알고리즘 스터디/20200128/1946_U.cpp
+++ 기타/알고리즘 스터디/20200128/1946_U.cpp
@@ -29,6 +29,8 @@ using namespace std;
 int T, N, a, b, tMax, answer;
 int v[100001][2];
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
     cin >> T;
     while(T--){
         tMax = 987654321;
@@ -50,3 +52,37 @@ int main(){
     }
 
 }
+
+
+import sys
+inp=sys.stdin.readline
+
+while(True):
+    try:
+        tc = int(inp())
+        break
+    except:
+        pass
+
+for i in range(tc):
+    while(True):
+        try:
+            n = int(inp())
+            break
+        except:
+            pass
+    a = []
+    n_input = 0
+    while(n_input<n):
+        temp = list(map(int,inp().split()))
+        if(len(temp) == 2):
+            a.append(temp)
+            n_input+=1
+    a.sort()
+    ans = 1
+    deg = a[0][1]
+    for j in range(1,len(a)):
+        if(deg > a[j][1]):
+            ans+=1
+            deg = a[j][1]
+    print (ans)

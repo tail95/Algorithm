b51101746f57fb59b65a68c90a9936baa51d16be
tail95 <xodlf8637@naver.com>
2020-03-24 00:04:07 +0900

    [BOJ]9372 상근이의 여행

--- 기타/9372.py
+++ 기타/9372.py
@@ -1,10 +1,18 @@
 import sys
+def check(i, N, answer):
+    for j in range(N):
+        if maps[i][j] == 1 and not visited[j]:
+            visited[j] = True
+            answer += 1
+            answer = check(j, N, answer)
+    return answer
+
 T = int(sys.stdin.readline())
 while T:
     answer = 0
     N, M = map(int, sys.stdin.readline().split())
     maps = [[0]*N for _ in range(N)]
-    visited = [0]*N
+    visited = [False]*N
     for _ in range(M):
         a, b = map(int, sys.stdin.readline().split())
         maps[a-1][b-1] = 1
@@ -12,6 +20,9 @@ while T:
         
     for i in range(N):
         for j in range(N):
-            
+            if maps[i][j] == 1 and not visited[i]:
+                visited[i] = True
+                answer = check(i, N, answer)
+    print(answer)
 
     T -= 1
